421de0fb5710821143415329d0eb6686221f5187
Ben <bennylopezhernandez0@gmail.com>
2025-07-10 22:35:03 -0400

    Update README

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Colors } from '@/constants/colors'
+import { Colors } from '@/constants/colors';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from 'expo-router';
+import { StyleSheet } from 'react-native';
 
 const TabLayout = () => {
   return (
     <Tabs
       screenOptions={{ tabBarActiveTintColor: Colors.navTint}}
     >
-      <Tabs.Screen />
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              color={color}
+              size={24}
+            />
+          )
+        }}
+      />
     </Tabs>
   )
 }
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  )
+}
+
+export default Index
+
+const styles = StyleSheet.create({})
